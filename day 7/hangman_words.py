da97fb9f83affcc36162d8e522a2897363581059
Felipe <felpcsarodrigues@gmail.com>
2024-10-19 20:31:02 -0300

    add day 6 and day 7

---
+++ day 7/hangman_words.py
@@ -0,0 +1,15 @@
+word_list = [
+    "cachorro", "gato", "elefante", "girafa", "tigre", "leao", "urso", 
+    "pinguim", "tartaruga", "cobra", "jacare", "carro", "moto", "bicicleta", 
+    "aviao", "helicoptero", "trem", "navio", "submarino", "computador", 
+    "teclado", "monitor", "telefone", "televisao", "lampada", "geladeira", 
+    "fogao", "armario", "sofá", "cadeira", "mesa", "janela", "porta", 
+    "livro", "caderno", "caneta", "lapis", "borracha", "pincel", "tinta", 
+    "praia", "montanha", "floresta", "deserto", "lago", "rio", "oceano", 
+    "chuva", "neve", "sol", "lua", "estrela", "planeta", "universo", 
+    "cidade", "pais", "estado", "capital", "futebol", "basquete", "volei", 
+    "tenis", "natacao", "corrida", "musica", "guitarra", "piano", "bateria", 
+    "violino", "filme", "teatro", "cinema", "ator", "atriz", "diretor", 
+    "jornal", "revista", "noticia", "reportagem", "historia", "geografia", 
+    "matematica", "fisica", "quimica", "biologia", "astronomia", "medicina"
+]
